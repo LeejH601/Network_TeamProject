8fbd13fd9b79ba42870fae1d71dfd70666130c05
LeejH601 <77761502+LeejH601@users.noreply.github.com>
2022-11-09 22:47:54 +0900

    Revert "Revert "ND RecvByNetwork 구현 테스트 미실시""
    This reverts commit c732ab710857a94ed34e39566c67bac19782f47c.

--- GALAGA_Server/NetworkDevice.h
+++ GALAGA_Server/NetworkDevice.h
@@ -10,9 +10,12 @@ class CNetworkDevice
 private:
 	SOCKET m_client_sock;
 
-	std::vector<std::vector<Telegram>> m_Telegrams;
+	std::vector<std::vector<Telegram>> m_SendTelegrams;
+	std::vector<std::vector<Telegram>> m_RecvTelegrams;
 
 public:
+	void init(SOCKET sock);
+
 	void SendToNetwork();
 	void RecvByNetwork();
 
